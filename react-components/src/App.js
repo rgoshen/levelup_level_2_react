502a661ab338d61e6927b9dde50a91b291b2c5f3
Richard Goshen <rick.goshen@gmail.com>
2022-07-19 17:35:30 -0700

    -m

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,23 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './Toggle';
+import Toggle from './ToggleRPC';
 import Portal from './Portal';
+import Modal from './Modal';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <Toggle>
           {({ on, toggle }) => (
             <Fragment>
-              {on && <h1>Show Me</h1>}
-              <button onClick={toggle}>Show / Hide</button>
-              <Portal>{on && <h1>Hi, I'm in a portal!</h1>}</Portal>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>Still in modal</h1>
+              </Modal>
             </Fragment>
           )}
         </Toggle>
